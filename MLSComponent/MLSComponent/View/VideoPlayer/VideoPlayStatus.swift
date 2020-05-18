280c8810e5627c181887fc668df5ea21ec43333c
Maxim Smirnov <>
2020-05-18 14:17:47 +0200

    SingleLineOverlayView

--- MLSComponent/MLSComponent/View/VideoPlayer/VideoPlayStatus.swift
+++ MLSComponent/MLSComponent/View/VideoPlayer/VideoPlayStatus.swift
@@ -2,29 +2,18 @@
 // Copyright © 2020 mycujoo. All rights reserved.
 //
 
-import Foundation
-
 enum VideoPlayStatus {
     
     case play
     case pause
     
     mutating func setOpposite() {
-        
         switch self {
-            
         case .play:
             self = .pause
-            
         case .pause:
             self = .play
-            
         }
-        
     }
-    
-    var isPlaying: Bool {
-        return self == .play
-    }
-    
+    var isPlaying: Bool { self == .play }
 }
