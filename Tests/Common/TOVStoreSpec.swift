d2a629760e8cfecac45c5e52460a67df4d925dae
Mats ten Bohmer <mats@mycujoo.tv>
2020-07-27 15:49:36 +0200

    Started to add tests for TOVStore

---
+++ Tests/Common/TOVStoreSpec.swift
@@ -0,0 +1,56 @@
+//
+// Copyright © 2020 mycujoo. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import MLSSDK
+
+
+class TOVStoreSpec: QuickSpec {
+
+    var tovStore: TOVStore!
+
+    override func setUp() {
+         continueAfterFailure = false
+    }
+
+    override func spec() {
+        beforeEach {
+            self.tovStore = TOVStore()
+        }
+
+        describe("saving and retrieving") {
+
+            it("saves and retrieves variables") {
+                let variables = [
+                    "variable1": TOVStore.Variable(name: "variable1", stringValue: "abc", doubleValue: nil, longValue: nil, doublePrecision: nil),
+                    "variable2": TOVStore.Variable(name: "variable2", stringValue: nil, doubleValue: 200.2828, longValue: nil, doublePrecision: 2),
+                    "variable3": TOVStore.Variable(name: "variable3", stringValue: nil, doubleValue: nil, longValue: 500, doublePrecision: nil),
+                ]
+
+                self.tovStore.new(variables: variables)
+
+                expect(self.tovStore.get(by: "variable1")?.humanFriendlyValue).to(equal(variables["variable1"]?.humanFriendlyValue))
+                expect(self.tovStore.get(by: "variable2")?.humanFriendlyValue).to(equal(variables["variable2"]?.humanFriendlyValue))
+                expect(self.tovStore.get(by: "variable3")?.humanFriendlyValue).to(equal(variables["variable3"]?.humanFriendlyValue))
+                expect(self.tovStore.get(by: "variable4")?.humanFriendlyValue).to(beNil())
+            }
+
+            it("saves and retrieves timers") {
+                let timers = [
+                    "timer1": TOVStore.Timer(name: "timer1", format: .s, direction: .up, startValue: 0),
+                    "timer2": TOVStore.Timer(name: "timer2", format: .ms, direction: .down, startValue: 270000),
+                ]
+
+                self.tovStore.new(timers: timers)
+
+                expect(self.tovStore.get(by: "timer1")?.humanFriendlyValue).to(equal(timers["timer1"]?.humanFriendlyValue))
+                expect(self.tovStore.get(by: "timer2")?.humanFriendlyValue).to(equal(timers["timer2"]?.humanFriendlyValue))
+                expect(self.tovStore.get(by: "timer3")?.humanFriendlyValue).to(beNil())
+            }
+        }
+    }
+}
+
