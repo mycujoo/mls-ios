85300593b9cb95f8cacd88e10fee468a2f67437c
Mats ten Bohmer <mats@mycujoo.tv>
2020-07-28 21:24:30 +0200

    Fix mocks

--- Tests/Common/VideoPlayerSpec.swift
+++ Tests/Common/VideoPlayerSpec.swift
@@ -32,19 +32,27 @@ class VideoPlayerSpec: QuickSpec {
             self.event = MLSSDK.Event(id: "mockevent", title: "Mock Event", descriptionText: "This is a mock event", thumbnailUrl: nil, organiser: nil, timezone: nil, startTime: Date().addingTimeInterval(-1 * 1000 * 3600 * 24), status: .started, streams: [MLSSDK.Stream(fullUrl: URL(string: "https://playlists.mycujoo.football/eu/ckc5yrypyhqg00hew7gyw9p34/master.m3u8")!)], timelineIds: [])
 
             self.mockAVPlayer = MockMLSAVPlayerProtocol()
-            self.mockAnnotationService = MockAnnotationServicing()
-
             stub(self.mockAVPlayer) { mock in
                 when(mock).addObserver(any(), forKeyPath: any(), options: any(), context: any()).thenDoNothing()
                 when(mock).addPeriodicTimeObserver(forInterval: any(), queue: any(), using: any()).thenReturn("")
                 when(mock).removeObserver(any(), forKeyPath: any()).thenDoNothing()
                 when(mock).removeTimeObserver(any()).thenDoNothing()
+                when(mock).currentDuration.get.thenReturn(200)
+                when(mock).currentTime.get.thenReturn(90)
+                when(mock).optimisticCurrentTime.get.thenReturn(100)
                 when(mock).isMuted.get.thenReturn(true)
                 when(mock).isMuted.set(any()).thenDoNothing()
                 when(mock).play().thenDoNothing()
                 when(mock).pause().thenDoNothing()
                 when(mock).replaceCurrentItem(with: any(), headers: any(), callback: any()).then { (tuple) in
                     (tuple.2)(true)
+                }
+            }
+
+            self.mockAnnotationService = MockAnnotationServicing()
+            stub(self.mockAnnotationService) { mock in
+                when(mock).evaluate(any(), callback: any()).then { (tuple) in
+                    (tuple.1)(AnnotationService.EvaluationOutput(showTimelineMarkers: [], showOverlays: [], hideOverlays: [], activeOverlayIds: Set(), tovs: [:]))
                 }
             }
 
@@ -61,7 +69,7 @@ class VideoPlayerSpec: QuickSpec {
 
             stub(self.mockAnnotationActionRepository) { mock in
                 when(mock).fetchAnnotationActions(byTimelineId: any(), callback: any()).then { tuple in
-                    // Should actually call the callback, but this is throwing an error.
+                    (tuple.1)([], nil)
                 }
             }
 
