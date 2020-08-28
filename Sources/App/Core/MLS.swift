f38ce0531c98abc403b2ea78db24b947059f222e
Mats ten Bohmer <mats@mycujoo.tv>
2020-08-28 16:23:50 +0200

    Added readme

--- Sources/App/Core/MLS.swift
+++ Sources/App/Core/MLS.swift
@@ -28,12 +28,6 @@ public class MLS {
 
     // TODO: Inject this dependency graph, rather than building it here.
 
-    #if DEBUG
-    private lazy var mockApi: MoyaProvider<API> = {
-        return MoyaProvider<API>(stubClosure: MoyaProvider.immediatelyStub)
-    }()
-    #endif
-
     private lazy var api: MoyaProvider<API> = {
         let authPlugin = AccessTokenPlugin(tokenClosure: { [weak self] _ in
             return self?.publicKey ?? ""
