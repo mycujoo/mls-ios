ac6c4be68389253721d78cd575488069290a258c
Mats ten Bohmer <mats@mycujoo.tv>
2020-10-15 17:48:50 +0200

    Add Fairplay to event/stream schema.

--- Sources/Domain/Entities/Public/Stream.swift
+++ Sources/Domain/Entities/Public/Stream.swift
@@ -7,10 +7,17 @@ import Foundation
 public struct Stream {
     public let id: String
     public let fullUrl: URL?
+    public let fairplay: FairplayStream?
 
-    public init(id: String, fullUrl: URL?) {
+    public init(id: String, fullUrl: URL?, fairplay: FairplayStream?) {
         self.id = id
         self.fullUrl = fullUrl
+        self.fairplay = fairplay
     }
 }
 
+public struct FairplayStream {
+    public let fullUrl: URL
+    public let licenseUrl: URL
+    public let certificateUrl: URL
+}
