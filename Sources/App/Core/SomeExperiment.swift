3e4810af9f5a03ea56291c9436dc1513cf7e4a7c
Mats ten Bohmer <mats@mycujoo.tv>
2020-10-20 23:39:43 +0200

    Initial SPM inclusion

---
+++ Sources/App/Core/SomeExperiment.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2020 mycujoo. All rights reserved.
+//
+
+import Foundation
+
+
+public struct SomeExperiment {
+    let a: String
+    let b: Int
+}
--- Sources/App/Utils/Bundle+.swift
+++ Sources/App/Utils/Bundle+.swift
@@ -6,11 +6,16 @@ import Foundation
 
 extension Bundle {
     static var resourceBundle: Bundle? {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
         let bundle = Bundle(for: MLS.self)
         guard let resourcesBundleUrl = bundle.resourceURL?.appendingPathComponent("MLSResources.bundle") else {
             // Fallback to the main bundle, which is only useful when compiling the framework directly.
             return bundle
         }
         return Bundle(url: resourcesBundleUrl) ?? bundle
+
+        #endif
     }
 }
