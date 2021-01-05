be85d1825b8af20ccc995e83306200b68cd830a1
Mats ten Bohmer <mats@mycujoo.tv>
2021-01-05 17:19:51 +0100

    Works and tested for tvos.

--- Examples/iOS/SimpleViewController.swift
+++ Examples/iOS/SimpleViewController.swift
@@ -10,7 +10,7 @@ import MLSSDK_IMA_iOS
 
 class SimpleViewController: UIViewController {
 
-    private lazy var mls = MLS(publicKey: "", configuration: Configuration())
+    private lazy var mls = MLS(publicKey: "", configuration: Configuration(playerConfig: PlayerConfig(imaAdUnit: "/124319096/external/single_ad_samples")))
 
     lazy var videoPlayer: VideoPlayer = {
         let player = mls.videoPlayer()
