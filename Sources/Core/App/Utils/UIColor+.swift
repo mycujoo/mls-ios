62c63664af82d0d11795b1041cc33a5653483d43
Mats ten Bohmer <mats@mycujoo.tv>
2021-01-14 12:38:37 +0100

    Also allow 8 character color codes.

--- Sources/Core/App/Utils/UIColor+.swift
+++ Sources/Core/App/Utils/UIColor+.swift
@@ -29,38 +29,49 @@ extension UIColor {
     convenience init(hex: String) {
         let scanner = Scanner(string: hex)
 
+        var fullLengthWithAlpha = 8
         var fullLength = 6
         var smallLength = 3
 
         if hex.hasPrefix("#") {
             scanner.scanLocation += 1
+            fullLengthWithAlpha += 1
             fullLength += 1
             smallLength += 1
         }
 
-        var rgb: UInt32 = 0
-        scanner.scanHexInt32(&rgb)
+        var code: UInt32 = 0
+        scanner.scanHexInt32(&code)
 
         var r: Int = 0
         var g: Int = 0
         var b: Int = 0
+        var a: Int = 0
 
         if hex.count == fullLength {
             let mask = 0x000000ff
-            r = Int(rgb >> 16) & mask
-            g = Int(rgb >>  8) & mask
-            b = Int(rgb >>  0) & mask
+            r = Int(code >> 16) & mask
+            g = Int(code >>  8) & mask
+            b = Int(code >>  0) & mask
+            a = 255
+        } else if hex.count == fullLengthWithAlpha {
+            let mask = 0x00000000ff
+            r = Int(code >> 24) & mask
+            g = Int(code >> 16) & mask
+            b = Int(code >>  8) & mask
+            a = Int(code >>  0) & mask
         } else if hex.count == smallLength {
             let mask = 0x000f
-            r = Int(rgb >> 8) & mask * 0x11
-            g = Int(rgb >> 4) & mask * 0x11
-            b = Int(rgb >> 0) & mask * 0x11
+            r = Int(code >> 8) & mask * 0x11
+            g = Int(code >> 4) & mask * 0x11
+            b = Int(code >> 0) & mask * 0x11
+            a = 255
         }
 
         self.init(red:   CGFloat(r) / 255,
                   green: CGFloat(g) / 255,
                   blue:  CGFloat(b) / 255,
-                  alpha: 1)
+                  alpha: CGFloat(a) / 255)
     }
 
     /// Compares two UIColor objects by their RGB integer values (it does not consider the alpha channel!).
