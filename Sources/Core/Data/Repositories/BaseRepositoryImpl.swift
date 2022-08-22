0cce8285b864556c8e557a3956b463ab83bec0ed
Mohammad Gharari <93533536+Ghararym@users.noreply.github.com>
2022-08-22 17:49:10 +0200

    add `_mutate` method

--- Sources/Core/Data/Repositories/BaseRepositoryImpl.swift
+++ Sources/Core/Data/Repositories/BaseRepositoryImpl.swift
@@ -28,4 +28,21 @@ class BaseRepositoryImpl {
             }
         }
     }
+    
+    func _mutate<T: Decodable>(_ endpoint: API, type t: T.Type, callback: @escaping (T?, Error?) -> ()) {
+        api.request(endpoint) { result in
+            switch result {
+            case .success(let response):
+                let decoder = JSONDecoder()
+                do {
+                    let data = try decoder.decode(t.self, from: response.data)
+                    callback(data, nil)
+                } catch {
+                    callback(nil, error)
+                }
+            case .failure(let error):
+                callback(nil, error)
+            }
+        }
+    }
 }
